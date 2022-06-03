441b3533cd9282bd962c143afe60f6468d12b352
danielware <waredanielb@gmail.com>
2022-06-03 10:31:31 -0700

    Added button fuctioning and scoring system for quiz.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,27 @@
 $(document).ready(function() {
-  $("form#info").submit(function(event) {
-  
-   
-
-
+  $("form#Quiz").submit(function(event) {
     event.preventDefault();
+    
+    const inputAge = parseInt($("#age").val());
+    const inputColor = $("#color").val();
+
+    let score = 0
+    if (inputAge <= 18) {
+      score + 5;
+    } else if (inputAge > 18 && inputAge<= 30) {
+      score += 10;
+    } else {
+      score += 15;
+    }
+    console.log(score);
+
+    if (score <=5 ) {
+      $('#C-Sharp').show();
+    } else if (score > 5 && score < 15) {
+      $('#Javascript').show();
+    } else if (score > 30) {
+      $('#Python').show();
+    }
+
   });
 });
