900480e4a387490c6edec6667ecf8d1be2f08bad
danielware <waredanielb@gmail.com>
2022-06-03 10:52:50 -0700

    Add second quiz question and update scoring.

--- js/scripts.js
+++ js/scripts.js
@@ -3,23 +3,37 @@ $(document).ready(function() {
     event.preventDefault();
     
     const inputAge = parseInt($("#age").val());
-    const inputColor = $("#color").val();
+    const inputColor = parseInt($("input:radio[name=color]:checked").val());
 
     let score = 0
+
+    // Age
     if (inputAge <= 18) {
-      score + 5;
-    } else if (inputAge > 18 && inputAge<= 30) {
+      score += 5;
+    } else if (inputAge > 18 && inputAge <= 30) {
       score += 10;
-    } else {
+    } else if (inputAge > 30 && inputAge <= 100) {
       score += 15;
     }
-    console.log(score);
+     else {
+       score += 0;
+    }
 
-    if (score <=5 ) {
+     //Color
+     if (inputColor === 1) {
+       score +=5;
+     } else if (inputColor === 2) {
+       score += 10;
+     } else {
+       score += 15;
+     }
+
+
+    if (score <=10 ) {
       $('#C-Sharp').show();
-    } else if (score > 5 && score < 15) {
+    } else if (score > 10 && score <= 20) {
       $('#Javascript').show();
-    } else if (score > 30) {
+    } else if (score > 20) {
       $('#Python').show();
     }
 
