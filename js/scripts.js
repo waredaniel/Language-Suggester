389e6cdb68b33beb3666e06ed4fbebea9f7d1abe
danielware <waredanielb@gmail.com>
2022-06-03 12:20:57 -0700

    Change result mechanism to replace text without adding answersbelow.

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,14 @@
 $(document).ready(function() {
   $("form#Quiz").submit(function(event) {
-    event.preventDefault();
     
     const inputAge = parseInt($("#age").val());
     const inputColor = parseInt($("input:radio[name=color]:checked").val());
     const inputAnimal = parseInt($("input:radio[name=animal]:checked").val());
     const inputTime = parseInt($("input:radio[name=time]:checked").val());
     const inputPlanet = parseInt($("input:radio[name=planet]:checked").val());
-
+    
     let score = 0
-
+    
     // Age
     if (inputAge <= 18) {
       score += 5;
@@ -18,28 +17,28 @@ $(document).ready(function() {
     } else if (inputAge > 30 && inputAge <= 100) {
       score += 15;
     }
-     else {
-       score += 0;
+    else {
+      score += 0;
     }
-
+    
     //Color
-     if (inputColor === 1) {
-       score +=5;
-     } else if (inputColor === 2) {
-       score += 10;
-     } else {
-       score += 15;
-     }
-
+    if (inputColor === 1) {
+      score +=5;
+    } else if (inputColor === 2) {
+      score += 10;
+    } else {
+      score += 15;
+    }
+    
     //Animal
-     if (inputAnimal === 1) {
+    if (inputAnimal === 1) {
       score +=5;
     } else if (inputAnimal === 2) {
       score += 10;
     } else {
       score += 15;
     }
-
+    
     //Time
     if (inputTime === 1) {
       score +=5;
@@ -48,7 +47,7 @@ $(document).ready(function() {
     } else {
       score += 15;
     }
-
+    
     //Planet
     if (inputPlanet === 1) {
       score +=5;
@@ -58,14 +57,29 @@ $(document).ready(function() {
       score += 15;
     }
     
-    //Score Results
-    if (score <=25 ) {
-      $('#C-Sharp').show();
-    } else if (score > 25 && score <= 50) {
-      $('#Javascript').show();
-    } else if (score > 50) {
-      $('#Python').show();
-    }
+    if (score <40 ) {
+       answerText = "Learn C-Sharp!";
+      } else if (score >= 40 && score < 60) {
+        answerText = "Learn JavaScript!";
+      } else if (score >= 60) {
+       answerText = "Learn Python!";
+      }
 
+    $("#result").show();
+    $("#result").text(answerText);
+
+
+
+
+    //Score Results
+    // if (score <40 ) {
+    //   $('#C-Sharp').show();
+    // } else if (score >= 40 && score < 60) {
+    //   $('#Javascript').show();
+    // } else if (score >= 60) {
+    //   $('#Python').show();
+    // }
+    
+    event.preventDefault();
   });
 });
